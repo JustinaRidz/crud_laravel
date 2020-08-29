4b11d3c468637b012378fee6de41b6c597f951b2
Justina <justina.ridz@gmail.com>
2020-08-29 18:57:42 +0300

    new layout/master & partials/header

---
+++ resources/views/jayouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    </head>
+    <body>
+        @include('partials.header')
+        <div class="container">
+            <div class="content">
+                @yield('content')
+            </div>
+        </div>
+        
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('/1', function () {
+//     return "<h1>Hello</h1>";
+// });
+
+// Route::get('/{text}', function ($text) {
+//     return $text;
+// });
+
+//… apkeiskime vietomis ir darykime užklausą į localhost/1
+
+
+// Route::get('/{text}', function($text) {
+//     return $text;
+// });
+
+
+// Route::get('//', function() {
+//     return '<div class="h1">Hello</div';
+// });
